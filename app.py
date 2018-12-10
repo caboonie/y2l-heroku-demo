c50121f05a04e0280652104b325fea68825eb5b6
caboonie <cnoble@mit.edu>
2018-12-10 11:52:03 -0500

    m

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("hello.html")
 
+@app.route('/test')
+def hello_world():
+    return "Test Boi"
+
 if __name__ == '__main__':
     app.run(debug=True)
